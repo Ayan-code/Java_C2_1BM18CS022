f02317ecb4d9a71777259fbce41568968eeb4d6f
Ayan-Decoder <53806596+Ayan-code@users.noreply.github.com>
2019-09-13 19:40:10 +0530

    Update Cust.java

--- Cust.java
+++ Cust.java
@@ -19,7 +19,7 @@ class Customer
   System.out.println("Enter name:");
   cname=sc.next();
   System.out.println("Enter customer number:");
-  cname=sc.nextInt();
+  cnum=sc.nextInt();
   System.out.println("Enter quantity:");
   qty=sc.nextInt();
   System.out.println("Enter price:");
@@ -39,5 +39,28 @@ class Customer
  }
  void display()
  {
-   
-  
+  System.out.println("Details:");
+  System.out.println("Customer name:"+cname);
+  System.out.println("Customer number:"+cnum);
+  System.out.println("Quantity:"+qty);
+  System.out.println("Price:"+price);
+  System.out.println("Total price:"+totprice);
+  System.out.println("Discount given:"+dis);
+  System.out.println("Net price:"+netprice);
+ }
+ class Cust
+ {
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the value of n:");
+    int n=sc.nextInt();
+    Customer C[]=new Customer[n];
+    for(int i=0;i<n;i++)
+    {
+     C[i]=new Customer();
+     C[i].get();
+     C[i].display();
+    }
+  }
+ } 
